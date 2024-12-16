850809857c7c1196e03f0f860d5c72f11bced29a
AHMET YAVUZ IŞIK <ayavuzisik@ogr.trakya.edu.tr>
2024-12-16 18:30:02 +0300

    HashType Done

---
+++ RedisApi.StackExchange/StackExchangeApi.Web/Controllers/BaseController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+using StackExchangeApi.Web.Services;
+
+namespace StackExchangeApi.Web.Controllers
+{
+    public class BaseController : Controller
+    {
+        private readonly RedisService _redisService;
+        protected IDatabaseAsync databaseAsync;
+        public BaseController(RedisService redisService)
+        {
+            _redisService = redisService;
+            databaseAsync = redisService.GetDatabase(4);
+        }
+    
+    }
+}
---
+++ RedisApi.StackExchange/StackExchangeApi.Web/Controllers/HashTypeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StackExchangeApi.Web.Services;
+
+namespace StackExchangeApi.Web.Controllers
+{
+    public class HashTypeController : BaseController
+    {
+        private readonly string hashKey = "dictionary";
+        public HashTypeController(RedisService redisService) : base(redisService)
+        {
+        }
+        public async Task<IActionResult> Index()
+        {
+            Dictionary<string, string> itemsDictionary = new Dictionary<string, string>();
+            if(await databaseAsync.KeyExistsAsync(hashKey))
+            {
+                var items = await databaseAsync.HashGetAllAsync(hashKey);
+                foreach (var item in items)
+                {
+                    itemsDictionary.Add(item.Name!, item.Value!);
+                }
+            }
+            return View(itemsDictionary);
+        }
+    
+    
+        public async Task<IActionResult> AddItem(string field, string value)
+        {
+            await databaseAsync.HashSetAsync(hashKey, field, value);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> DeleteItem(string field)
+        {
+            await databaseAsync.HashDeleteAsync(hashKey, field);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
