ac4418b9711c3fe30a0a40f7f231e3f319f5e34d
AHMET YAVUZ IŞIK <ayavuzisik@ogr.trakya.edu.tr>
2025-01-04 04:17:56 +0300

    Redis Connection clib

---
+++ RedisConnect/RedisService.cs
@@ -0,0 +1,23 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisConnect
+{
+    public class RedisService 
+    {
+        private readonly ConnectionMultiplexer _connectionMultiplexer;
+        public RedisService(string url)
+        {
+            _connectionMultiplexer = ConnectionMultiplexer.Connect(url);
+        }
+
+        public IDatabaseAsync GetDatabaseAsync(int db)
+        {
+            return _connectionMultiplexer.GetDatabase(db);
+        }
+    }
+}
